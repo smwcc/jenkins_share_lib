7376cdfea81b13382af9f41c532d8369452044d1
smwcc <72016364+smwcc@users.noreply.github.com>
2020-10-06 20:32:21 +0800

    Update gitlab.groovy

--- src/org/devops/gitlab.groovy
+++ src/org/devops/gitlab.groovy
@@ -3,7 +3,7 @@ package org.devops
 //封装HTTP请求
 def HttpReq(reqType,reqUrl,reqBody){
     def gitServer = "http://192.168.16.130:8000/api/v4"
-    withCredentials([string(credentialsId: 'gitlab-token', variable: 'gitlabToken')]) {
+    withCredentials([string(credentialsId: 'gitlab-token', variable: 'GitLab API token')]) {
       result = httpRequest customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: "${gitlabToken}"]], 
                 httpMode: reqType, 
                 contentType: "APPLICATION_JSON",
